619bd0e1833327a156ef1981920581bbdcb00738
Bishal Humagain <humagainbishal02@gmail.com>
2026-08-11 19:49:06 +0545

    update file

--- HeartAttack.py
+++ HeartAttack.py
@@ -1,2 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
+df= pd.read_csv(r"C:\Users\humag\OneDrive\Desktop\HeartAttackAnalysis\Heart Attack Data Set.csv")
+print(df.describe())
+print(df.info())
