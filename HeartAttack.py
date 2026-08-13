324a454b4a82af3b7d931fee24478497ed61848d
Bishal Humagain <humagainbishal02@gmail.com>
2026-08-13 16:38:54 +0545

    image problem solved

--- HeartAttack.py
+++ HeartAttack.py
@@ -12,21 +12,25 @@ print(df.head())
 
 sns.countplot(x='target', data=df)
 plt.title('count of target variable')
-plt.show()
-# plt.savefig('count_of_target_variable.png')
+
+plt.savefig('HeartAttack/count_of_target_variable.png')
+plt.close()
 
 sns.boxplot(x='target', y='age', data=df)
 plt.title('boxplot of age by target')
-plt.show()
-# plt.savefig('boxplot_of_age_by_target.png')
+# plt.show()
+plt.savefig('HeartAttack/boxplot_of_age_by_target.png')
+plt.close()
 
 sns.countplot(x='sex', hue='target', data=df)
 plt.title('count of sex by target')
-plt.show()
-plt.savefig('count_of_sex_by_target.png')
+# plt.show()
+plt.savefig('HeartAttack/count_of_sex_by_target.png')
+plt.close()
 
 plt.figure(figsize=(12,8))
 sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
-plt.show()
-# plt.savefig('correlation_heatmap.png')
+# plt.show()
+plt.savefig('HeartAttack/correlation_heatmap.png')
+plt.close()
 
